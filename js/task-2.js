7ae8e70a68741abd27805a4ce81d29cef39d6a38
Arif Yazici <tercihtl.com@gmail.com>
2026-03-31 02:03:25 +0300

    Refactor makeArray function and add tests
    Updated test cases for makeArray function to include a variety of inputs.

--- js/task-2.js
+++ js/task-2.js
@@ -1,21 +1,24 @@
 function makeArray(firstArray, secondArray, maxLength) {
-  // 1. İki diziyi birleştiriyoruz (concat metodu ile)
   const allArray = firstArray.concat(secondArray);
 
-  // 2. Uzunluk kontrolü yapıyoruz
   if (allArray.length > maxLength) {
-    // Eğer uzunluk sınırı aşıyorsa, 0'dan maxLength'e kadar olan kısmı alıyoruz
     return allArray.slice(0, maxLength);
   }
 
-  // 3. Sınırı aşmıyorsa tamamını döndürüyoruz
   return allArray;
 }
 
+// 1. Test
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); 
 
-// Kontrol testlerini altına ekle
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango", "Ajax", "Chelsea", "Poly", "Houston"], 3)); // Hata almamak için 2. parametreyi unutma!
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+// 2. Test
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); 
+
+// 3. Test (DÜZELTİLEN YER: Buraya iki ayrı dizi ve bir sayı ekledik)
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); 
+
+// 4. Test
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); 
+
+// 5. Test
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
